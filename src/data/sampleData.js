69f47b61b52cb7a04c8671e58c8a74f5bd77ef1c
Peter Cheng <petercheng7788@gmail.com>
2020-08-16 23:17:44 +0800

    uploaded live and test data

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -1,12 +1,28 @@
 const sampleData = [
   {
     pid: 1,
-    name: "Gundam Maxi Boost On",
+    name: "Gundam Maxi Boost On Live",
+    description: "fun game",
+    //sku: "price_1HGnGCIEWLQJp0xozegOuECg",
+    sku: "price_1HGnGWIEWLQJp0xoz1UirVZ1",
+    price: 5000,
+    currency: "hkd",
+    //product: "prod_HqUEnzQQIpbUuK",
+    product: "prod_HqUFbL1BbGq8oX",
+    image:
+      "https://www.fortress.com.hk/medias/12058138-12058138.jpg?context=bWFzdGVyfGZyb250cHJkfDE4MjI0N3xpbWFnZS9qcGVnfGZyb250cHJkL2hkYi9oNzAvOTUyMjU4OTYzMDQ5NC5qcGd8NmM2MTdjMmZlOGI0ZTIyOTk1MjhjODE5ZmM4NDhjZTgwNDJlNjkxM2YzZDEwOWU5MTA1NDExZWE5ZTE3ODhjYg",
+  },
+
+  {
+    pid: 2,
+    name: "Gundam Maxi Boost On Test",
     description: "fun game",
     sku: "price_1HGnGCIEWLQJp0xozegOuECg",
+    // sku: "price_1HGnGWIEWLQJp0xoz1UirVZ1",
     price: 5000,
     currency: "hkd",
     product: "prod_HqUEnzQQIpbUuK",
+    // product: "prod_HqUFbL1BbGq8oX",
     image:
       "https://www.fortress.com.hk/medias/12058138-12058138.jpg?context=bWFzdGVyfGZyb250cHJkfDE4MjI0N3xpbWFnZS9qcGVnfGZyb250cHJkL2hkYi9oNzAvOTUyMjU4OTYzMDQ5NC5qcGd8NmM2MTdjMmZlOGI0ZTIyOTk1MjhjODE5ZmM4NDhjZTgwNDJlNjkxM2YzZDEwOWU5MTA1NDExZWE5ZTE3ODhjYg",
   },
